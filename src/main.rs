2ed01aa85fc0e7809fc0ba63bf80bc68d77659f6
谈谈 <tanjiarui123@live.com>
2024-08-15 10:53:50 +0800

    初步错误处理

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+// use std::io::{self, Read};
+// use std::fs;
+use std::io::{self,stdout,Read};
+use termion::raw::IntoRawMode;
+// use std::io::Read
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e:std::io::Error) {
+    panic!("{}", e)
+}
+
+
+fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        if b == to_ctrl_byte('d'){
+            break;
+        }
+    }
+    
+}
