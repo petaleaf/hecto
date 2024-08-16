58b5e194b341835697cecc11c6d9f5d3ff47fd5a
谈谈 <tanjiarui123@live.com>
2024-08-16 11:14:59 +0800

    关注点分离：将终端相关的东西全部移动到terminal.rs

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,6 @@
 use crate::Terminal;
-use std::io::{self,stdout,Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
 
 
 pub struct Editor{
@@ -12,7 +10,6 @@ pub struct Editor{
 impl Editor{
 
     pub fn run(&mut self){
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
             if let Err(error)= self.refresh_screen(){
                 die(&error);
@@ -44,20 +41,22 @@ impl Editor{
     fn refresh_screen(&self) -> Result<(),std::io::Error>{
         // print!("\x1b[2J");
         // 清空屏幕，并将光标放在左上角
-        print!("{}{}",termion::clear::All, termion::cursor::Goto(1,1));
+        Terminal::clear_screen();
+        Terminal::cursor_position(0,0);
         // 打印退出信息
         if self.should_quit{
             println!("Goodbye.\r");
         }else {
             self.draw_rows();
-            print!("{}",termion::cursor::Goto(1,1))
+            Terminal::cursor_position(0,0);
+            
         }
 
-        io::stdout().flush()
+        Terminal::flush()
     }
     // 对输入的信息进行处理
     fn process_keypress(&mut self)->Result<(),std::io::Error>{
-        let pressed_key = Self::read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             // Key::Ctrl('d') =>panic!("Promgram end"),
             Key::Ctrl('d') => self.should_quit = true,
@@ -73,19 +72,11 @@ impl Editor{
     }
 
 
-    // 读取输入
-    fn read_key() -> Result<Key, std::io::Error> {
-        loop {
-            if let Some(key) = io::stdin().lock().keys().next() {
-                return key;
-            }
-        }
-    }
-
+    
 }
 
 fn die(e: &std::io::Error) {
     // 发生错误时清空屏幕，打印错误
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}",e);
 }
