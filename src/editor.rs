cf68cfc7dd1dc96943b240fb1ba8b3d2d0e46fc2
谈谈 <tanjiarui123@live.com>
2024-08-16 12:15:59 +0800

    添加欢迎信息

--- src/editor.rs
+++ src/editor.rs
@@ -2,11 +2,19 @@ use crate::Terminal;
 use termion::event::Key;
 
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+pub struct Position {
+    x: usize,
+    y: usize,
+}
 pub struct Editor{
     should_quit: bool,
     terminal:Terminal,
+    cursor_position:Position,
 }
+
+
 impl Editor{
 
     pub fn run(&mut self){
@@ -14,14 +22,15 @@ impl Editor{
             if let Err(error)= self.refresh_screen(){
                 die(&error);
             }
+            
+            if self.should_quit {
+                break;
+            }
 
             if let Err(error) = self.process_keypress(){
                 die(&error);
             }
 
-            if self.should_quit {
-                break;
-            }
             
         }
     }
@@ -35,23 +44,26 @@ impl Editor{
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 },
          }
     }
     // 刷新屏幕
     fn refresh_screen(&self) -> Result<(),std::io::Error>{
         // print!("\x1b[2J");
         // 清空屏幕，并将光标放在左上角
-        Terminal::clear_screen();
+        Terminal::cursor_hide();
+        // Terminal::clear_screen();
         Terminal::cursor_position(0,0);
         // 打印退出信息
         if self.should_quit{
+            Terminal::clear_screen();
             println!("Goodbye.\r");
         }else {
             self.draw_rows();
             Terminal::cursor_position(0,0);
             
         }
-
+        Terminal::cursor_show();
         Terminal::flush()
     }
     // 对输入的信息进行处理
@@ -65,9 +77,34 @@ impl Editor{
         Ok(())
 
     }
+    fn draw_welcome_message(&self){
+        let mut welcome_message = format!("Hecto editor -- version {}",VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}",spaces,welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r",welcome_message);
+    }
+
+
+
     fn draw_rows(&self){
-        for _ in 0..self.terminal.size().height{
-            println!("~\r");
+        // 避免左后一行没有波浪线
+        
+        let height = self.terminal.size().height;
+        for row in 0..height -1{
+            // println!("~\r");
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+                // let welcome_message = format!("Hecto editor -- version {}",VERSION);
+                // let width = std::cmp::min(self.terminal.size().width as usize, welcome_message.len(),);
+                // println!("{}\r",&welcome_message[..width]);
+            } else{
+                println!("~\r");
+            }
         }
     }
 
