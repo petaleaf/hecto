a833e0477d33cf8f7b571f652d452b299be7196d
谈谈 <tanjiarui123@live.com>
2024-08-20 16:02:29 +0800

    初步修复光标移动不按照字位移动

--- src/row.rs
+++ src/row.rs
@@ -1,13 +1,20 @@
 use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl From<&str> for Row{
     fn from(slice: &str) ->Self {
-        Self {
-            string: String::from(slice),
-        }
+        // Self {
+            let mut row = Self{
+                string: String::from(slice),
+                len: 0,
+            };
+            row.update_len();
+            row
+        // }
     }
 }
 
@@ -18,13 +25,30 @@ impl Row {
         let start = cmp::min(start, end);
         // unwarp_or_default() 返回无效时返回默认的空（）
         // self.string.get(start..end).unwrap_or_default().to_string()
-        self.string.get(start..end).unwrap_or_default().to_string()
+        // self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            result.push_str(grapheme);
+        }
+        result
     }
     // 获取水平方向的长度
     pub fn len(&self) ->usize{
-        self.string.len()
+        // self.string.len()
+        // 按照我看看到的字计数，即多少字位
+        // self.string[..].graphemes(true).count()
+        self.len
     }
     pub fn is_empty(&self) ->bool{
-        self.string.is_empty()
+        // self.string.is_empty()
+        self.len == 0
+    }
+    // 在此处单独是实现，可以避免count便利整个迭代器，这样可以减少资源消耗
+    fn update_len(&mut self){
+        self.len = self.string[..].graphemes(true).count();
     }
 }
