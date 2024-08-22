8441eee9d76e0669a27c9f5ab8a0e141782d6e23
谈谈 <tanjiarui123@live.com>
2024-08-22 15:33:17 +0800

    实现输入字符光标向右移动

--- src/editor.rs
+++ src/editor.rs
@@ -132,7 +132,11 @@ impl Editor{
         match pressed_key {
             // Key::Ctrl('d') =>panic!("Promgram end"),
             Key::Ctrl('d') => self.should_quit = true,
-            Key::Char('c') => self.document.insert(&self.cursor_position, c),
+            // Key::Char(c) => self.document.insert(&self.cursor_position, c),
+            Key::Char(c) =>{
+                self.document.insert(&self.cursor_position, c);
+                self.move_cursor(Key::Right);
+            }
             Key::Up 
             | Key::Down 
             | Key::Left 
